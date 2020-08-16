14518f8c103c17c4968e22445e31f61708a4fc4c
bylmwoo1 <49145529+bylmwoo1@users.noreply.github.com>
2020-08-16 18:28:58 +0900

    Update NoticeController.java

--- HelloGit/src/main/java/com/kh/hellogit/notice/controller/NoticeController.java
+++ HelloGit/src/main/java/com/kh/hellogit/notice/controller/NoticeController.java
@@ -4,5 +4,9 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class NoticeController {
+  @RequestMapping("nlist.do")
+  public String noticeListSelect(){
+    return "home";
+  }
 
 }
